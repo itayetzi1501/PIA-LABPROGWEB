62f2761cd1263f9368182bea80eb59ceace298f3
itayetzi1501 <133730730+itayetzi1501@users.noreply.github.com>
2024-05-27 22:14:06 -0600

    modificaciones

--- crearcuenta.php
+++ crearcuenta.php
@@ -9,21 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $conn->real_escape_string($_POST['Nombre']);
     $apellido_paterno = $conn->real_escape_string($_POST['ApellidoPaterno']);
     $apellido_materno = $conn->real_escape_string($_POST['ApellidoMaterno']);
-    $email = $conn->real_escape_string($_POST['correo']);
-    $telefono = $conn->real_escape_string($_POST['telefono']);
-    $direccion = $conn->real_escape_string($_POST['direccion']);
+    $email = $conn->real_escape_string($_POST['Correo']);
+    $telefono = $conn->real_escape_string($_POST['Telefono']);
+    $direccion = $conn->real_escape_string($_POST['Direccion']);
     $cp = $conn->real_escape_string($_POST['CodigoPostal']);
-    $fecha_nacimiento = $conn->real_escape_string($_POST['FechaNacimiento']);
+    //$fecha_nacimiento = $conn->real_escape_string($_POST['FechaNacimiento']);
+    //$fecha=DateTime::createFormFormat('d/m/Y', $fecha_nacimiento);
+    //$fechanueva=$fecha->format('Y-m-d')
+
     $password = password_hash($conn->real_escape_string($_POST['Contraseña']), PASSWORD_BCRYPT);
 
     $sql_check = "SELECT * FROM usuarios WHERE username = '$Id_Usuario' OR email = '$Correo'";
     $result_check = $conn->query($sql_check);
-
+    
     if ($result_check->num_rows > 0) {
         $error = "El usuario o correo electrónico ya existe";
     } else {
-        $sql = "INSERT INTO usuarios (username, Nombre, ApellidoPaterno, ApellidoMaterno, Correo, Telefono, Direccion, CodigoPostal, FechaNacimiento, Contraseña) 
-                VALUES ('$username', '$Nombre', '$ApellidoPaterno', '$ApellidoMaterno', '$Coreo', '$Telefono', '$Direccion', '$CodigoPostal', '$FechaNacimiento', 'Contraseña')";
+        $sql = "INSERT INTO usuarios (Nombre, ApellidoPaterno, ApellidoMaterno, Correo, Telefono, Direccion, CodigoPostal, FechaNacimiento, Contraseña) 
+                VALUES ('$nombre', '$apellido_paterno', '$apellido_materno', '$email', $telefono, '$direccion', $cp, '2003-11-01', '$password')";
 
         if ($conn->query($sql) === TRUE) {
             $success = "Cuenta creada exitosamente. Ahora puedes <a href='login.php'>iniciar sesión</a>";
